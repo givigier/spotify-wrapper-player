79c4eed415076fd52be5ca02bf0c307d84930fb3
Gabriel Givigier <gabriel@givigier.com>
2018-01-22 10:48:47 -0200

    Import spotify-wrapper

---
+++ src/Spotify.js
@@ -0,0 +1,6 @@
+import SpotifyWrapper from 'spotify-wrapper';
+const spotify = new SpotifyWrapper({
+  token: 'BQCppDE4FPEFb7n_WjsgL0RkiAseVwL0hrMHojodERzH3ulMMWm-VkWjrvGPcxsGoP3vEmXPuTT_DZj3FlHgjvEIEOUjNLZfCQlDXxtd1__2FE_654lEXm4EeeGnPPhXMN5gFnXgV7T8jXw',
+});
+
+export default spotify;
