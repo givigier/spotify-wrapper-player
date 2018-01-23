2ef2407715c62a53b8b3e475e1cecfb527966d9b
Gabriel Givigier <gabriel@givigier.com>
2018-01-23 11:35:37 -0200

    Add event triggers

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,6 +1,6 @@
 import SpotifyWrapper from 'spotify-wrapper';
 const spotify = new SpotifyWrapper({
-  token: 'BQAXTYDWu2KCalyF1n8Dve9bjOa0lPXVJ4jIXyLhA5XnK3NCmCoop01QSk7wLnNby94niI3QwYRRhZqyA4bIcW2DoIgn40-kaxFNvocEoabqfmXz4UGQiPjvJDFOZHpUI144d-f4au_kTC0',
+  token: '',
 });
 
 export default spotify;
--- src/main.js
+++ src/main.js
@@ -1,17 +1,7 @@
-import spotify from './Spotify';
-import renderAlbums from './AlbumList';
-import renderAlbumInfo from './AlbumInfo';
-import renderAlbumTracks from './AlbumTracks';
+import searchEnterTrigger from './SearchTrigger';
+import selectAlbumTrigger from './SelectAlbumTrigger';
+import playlistTrigger from './PlaylistTrigger';
 
-const albums = spotify.search.albums('Beirut');
-const albumList = document.getElementById('album-list');
-
-const album = spotify.album.getAlbum('5N82AYJ3FG1znFz3EqJNMm');
-const albumInfo = document.getElementById('album-info');
-
-const albumTracks = document.getElementById('album-tracks');
-
-albums.then(data => renderAlbums(data.albums.items, albumList));
-album
-  .then(data => renderAlbumInfo(data, albumInfo))
-  .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
+searchEnterTrigger();
+selectAlbumTrigger();
+playlistTrigger();
