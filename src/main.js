558adc5857e5cab0cd53b0f0bbfac632541cc1d5
Gabriel Givigier <gabriel@givigier.com>
2018-01-23 09:22:56 -0200

    Show album info

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import spotify from './Spotify';
 import renderAlbums from './AlbumList';
+import renderAlbumInfo from './AlbumInfo';
 
 const albums = spotify.search.albums('Beirut');
 const albumList = document.getElementById('album-list');
 
+const album = spotify.album.getAlbum('5N82AYJ3FG1znFz3EqJNMm');
+const albumInfo = document.getElementById('album-info');
+
 albums.then(data => renderAlbums(data.albums.items, albumList));
+album.then(data => renderAlbumInfo(data, albumInfo));
